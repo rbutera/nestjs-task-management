ae2953372739a6e4b90eb1cc4a19ace100bc02f4
Rai Butera <rai@rbutera.com>
2020-04-30 18:27:06 +0100

    add section 2 methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common'
+import {TasksService} from './tasks.service'
+import {Task} from './task.model'
+import {CreateTaskDto} from './dto/create-task.dto'
+import {UpdateTaskStatusDto} from './dto/update-task-status.dto'
+import {GetTasksFilterDto} from './dto/get-tasks-filter.dto'
+import {isEmpty} from 'ramda'
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (!isEmpty(filterDto)) {
+      return this.tasksService.getTasksWithFilter(filterDto)
+    }
+
+    return this.tasksService.getAllTasks()
+  }
+
+  @Get('/:id')
+  getTask(@Param('id') id: string): Task {
+    return this.tasksService.getTask(id)
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id)
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, updateTaskStatusDto)
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto)
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common'
+import {Task, TaskStatus} from './task.model'
+import {v1 as uuid} from 'uuid'
+import {CreateTaskDto} from './dto/create-task.dto'
+import {UpdateTaskStatusDto} from './dto/update-task-status.dto'
+import {GetTasksFilterDto} from './dto/get-tasks-filter.dto'
+import {contains} from 'ramda'
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = []
+
+  getAllTasks(): Task[] {
+    return this.tasks
+  }
+
+  getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+    const {status, search} = filterDto
+    const containsSearchTerm = contains(search)
+    return this.tasks.filter(
+      task =>
+        (status && task.status === status) ||
+        (search &&
+          (containsSearchTerm(task.description) ||
+            containsSearchTerm(task.title)))
+    )
+  }
+
+  getTask(id: string): Task {
+    const [result] = this.tasks.filter(task => task.id === id)
+    return result
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter(task => task.id !== id)
+  }
+
+  updateTaskStatus(id: string, updateTaskStatusDto: UpdateTaskStatusDto): Task {
+    const {status} = updateTaskStatusDto
+    this.tasks = this.tasks.map(task => ({
+      ...task,
+      status: task.id === id ? status : task.status,
+    }))
+
+    const [updated] = this.tasks.filter(task => task.id === id)
+    return updated
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const {title, description} = createTaskDto
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    }
+
+    this.tasks = [...this.tasks, task]
+
+    return task
+  }
+}
